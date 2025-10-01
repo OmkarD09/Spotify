e5056e42120243ab593fa44a3ca9b4d493dcc8e5
OmkarD09 <omkardhakane09@gmail.com>
2025-10-02 00:08:57 +0530

    few changes

---
+++ script.js
@@ -0,0 +1,37 @@
+let url = "http://127.0.0.1:3000/songs/";
+
+async function getSongs() {
+
+        let a = await fetch(url);
+        let response = await a.text();
+        console.log(response); 
+        let div = document.createElement("div");
+        div.innerHTML = response;
+        let as = div.getElementsByTagName("a");
+    let songs = [];
+        for (let i = 0; i < as.length; i++) {
+            const element = as[i];
+            if(element.href.endsWith(".mp3")){
+                songs.push(element.href);
+            }
+        }
+    return songs;
+}
+
+
+async function main() {
+    let songs = await getSongs();
+    console.log(songs);
+    
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+     let duration = audio.duration; // Duration in seconds
+     console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    }
+    );
+}
+
+main();
+
