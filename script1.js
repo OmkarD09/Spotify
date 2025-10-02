e99815af5533abd621d64d8b7425457dc7f8492f
OmkarD09 <omkardhakane09@gmail.com>
2025-10-02 12:30:59 +0530

    lala

---
+++ script1.js
@@ -0,0 +1,40 @@
+let url = "http://127.0.0.1:3000/songs/";
+
+async function getSongs() {
+
+        let a = await fetch(url);
+        let response = await a.text();
+        console.log(response); 
+        let div = document.createElement("div");
+        div.innerHTML = response;
+        let as = div.getElementsByTagName("a");
+    let songs = [];
+        for (let i = 0; i < as.length; i++) {
+            const element = as[i];
+            if(element.href.endsWith(".mp3")){
+                songs.push(element.href);
+            }
+        }
+    return songs;
+}
+
+async function main() {
+    let songs = await getSongs();
+    console.log(songs);
+    
+    let songUL = document.querySelector("#songListol");
+    console.log(songUL);
+
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + song;
+    }
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+     let duration = audio.duration; // Duration in seconds
+     console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    }
+    );
+}
